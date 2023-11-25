1b5624e9ff3160ca98f0455a2654971ab1fe33c8
Francisco Emanuel <emanuel.cunha678@gmail.com>
2023-11-25 09:48:56 -0300

    commit 1

---
+++ script.js
@@ -0,0 +1,16 @@
+function openTab(event, tabName) {
+    var i, tabContent, tablinks;
+
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
