887e099f770dad1f6051c515029662e1444ad03a
Francisco Emanuel <emanuel.cunha678@gmail.com>
2023-12-01 18:30:40 -0300

    commit 2

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ function openTab(event, tabName) {
     document.getElementById(tabName).style.display = "block";
     event.currentTarget.className += " active";
 }
+// function Troca (){
+//     document.getElementById("nav").innerHTML = (`<a class="ifpi" href="https://www.ifpi.edu.br/parnaiba"><img src="img/ifpi.png" width="115px" align="center"></a>
+//     <span class="links">
+//         <a class="nav-link" href="index.html">Home</a>
+//         <a class="nav-link" href="#">Educação Física e Saúde</a>
+//         <a class="nav-link" href="sobre-nos.html">About Us</a>
+//     </span>
+//     <span class="links-2">
+//         <a class="nav-link" href="" ><img src="img/united-states-of-america.png" width="30px" alt="" align="center">&nbsp;PT</a>
+//         <a class="nav-login" href="login.html" on><img src="img/login.png" alt="" width="1.8%" align="center">&nbsp;Login</a>
+//     </span>`);
+// }
